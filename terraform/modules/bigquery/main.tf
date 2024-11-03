acaab83aae958898c0dc2759f6f49e85959bd12c
IMISSHER99 <145341240+IMISSHER99@users.noreply.github.com>
2024-11-03 11:21:19 +0530

    [FEATURE] Add BigQuery Dataset/Table Creation and Update GitHub Actions for Dataflow Code Deployment (#13)
    * Terraform script for GCS Completed
    
    * Completed dataflow development
    
    * Pushed changes
    
    * removed unsed varible
    
    * Completed CI-CD setup, Need to test
    
    * Changed backend bucket name
    
    * Added missing env varibales
    
    * Created bigquery terraform script and updated CI-CD env varibales
    
    * Changed schema path
    
    * Changed schema path
    
    * Changed schema path
    
    * Updated bigquery table to reference the dataset id
    
    * Added depends on
    
    * Changed schema, Added step to transfer dataflow code to cloud storage
    
    * Added key logs to check the error
    
    * Reverted back key logs
    
    * Checking active account
    
    * Added steps to setup gcloud sdk
    
    * Testing if owner access lets you view the table
    
    * Added new Owner and created a variable for it
    
    * Updated CI-CD for new environment variable

---
+++ terraform/modules/bigquery/main.tf
@@ -0,0 +1,39 @@
+resource "google_bigquery_dataset" "bigquery_dataset" {
+  dataset_id = var.dataset_name
+  description = var.dataset_description
+  location = var.project_region
+  project = var.project_id
+
+  access {
+    role = var.role_owner
+    user_by_email = var.service_account_name
+  }
+
+  access {
+    role = var.role_owner
+    user_by_email = var.owner_email
+  }
+
+  labels = {
+    environment = var.environment
+  }
+
+  delete_contents_on_destroy = true
+
+}
+
+
+resource "google_bigquery_table" "bigquery_table" {
+  depends_on = [google_bigquery_dataset.bigquery_dataset]
+  dataset_id = google_bigquery_dataset.bigquery_dataset.dataset_id
+  table_id   = var.table_name
+  project = var.project_id
+  description = var.table_description
+  schema = file(var.schema_file_path)
+
+  labels = {
+    environment = var.environment
+  }
+
+  deletion_protection = false
+}
