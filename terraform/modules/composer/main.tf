9fe08edbd8a06af3c465c4a1265420bbd9930e94
IMISSHER99 <145341240+IMISSHER99@users.noreply.github.com>
2024-11-04 15:04:11 +0530

    [FEATURE] Complete Airflow Setup with Artifactory Integration and Dataflow DAG Creation (#16)
    * Terraform script for GCS Completed
    
    * Completed dataflow development
    
    * Pushed changes
    
    * removed unsed varible
    
    * Completed CI-CD setup, Need to test
    
    * Changed backend bucket name
    
    * Added missing env varibales
    
    * Created bigquery terraform script and updated CI-CD env varibales
    
    * Changed schema path
    
    * Changed schema path
    
    * Changed schema path
    
    * Updated bigquery table to reference the dataset id
    
    * Added depends on
    
    * Changed schema, Added step to transfer dataflow code to cloud storage
    
    * Added key logs to check the error
    
    * Reverted back key logs
    
    * Checking active account
    
    * Added steps to setup gcloud sdk
    
    * Testing if owner access lets you view the table
    
    * Added new Owner and created a variable for it
    
    * Updated CI-CD for new environment variable
    
    * Created Composer, new bucket for composer
    
    * Added new envionment varibales for the composer and its bucket
    
    * Changed bucket name typo
    
    * Added steps to zip and push the file to cloud storage
    
    * Added steps to zip and push the file to cloud storage
    
    * Added new step to push image to artifactory
    
    * Added new step to push image to artifactory
    
    * Changed type
    
    * Changed image environment varible
    
    * Changed dockerfile path
    
    * Changed dockerfile path
    
    * Changed dockerfile path
    
    * Created Docker to push image to artifcatory
    
    * Removed extra space from dockerfile
    
    * Updated Schema for bigquery
    
    * Added Artifcatory, created dag, added new steps to transfer dag to GCS
    
    * Fixed typo in actions yaml
    
    * added env varibales to actions and terraform
    
    * Fixed config cluster name issue
    
    * Fixed env varibale issue
    
    * Added region and project to the data block
    
    * Added region and project to the data block
    
    * Updated Actions to output cluster name
    
    * added cluster_name output
    
    * Getting cluster name
    
    * Added step for GKE Auth
    
    * Added step to create namesspace and service account only if they do not exists
    
    * changed env variable name
    
    * Airflow setup completed

---
+++ terraform/modules/composer/main.tf
@@ -0,0 +1,29 @@
+resource "google_composer_environment" "composer" {
+  name    = var.composer_name
+  region  = var.project_region
+  project = var.project_id
+
+  storage_config {
+    bucket = var.composer_bucket_name
+  }
+
+  config {
+    node_config {
+      service_account = var.service_account_name
+    }
+    software_config {
+      image_version = var.composer_image_version
+      env_variables = {
+        DATASET_BUCKET_NAME: var.dataset_bucket_name
+        SOURCE_FOLDER: var.source_folder_path
+        ARCHIVE_FOLDER: var.archive_folder_path
+        GCP_PROJECT_ID: var.project_id
+        GCP_PROJECT_REGION: var.project_region
+        CLUSTER_NAMESPACE: var.cluster_namespace
+        CLUSTER_SERVICE_ACCOUNT_NAME: var.cluster_service_account_name
+        ARTIFACTORY_IMAGE_NAME: var.artifactory_image_name
+      }
+    }
+
+  }
+}
